e184f2f7898c93664c81ed5e475a369b2f60d064
BrunoXBSantos <brunobsantos.04@gmail.com>
2020-04-27 14:20:02 +0100

    Server

--- src/Server.java
+++ src/Server.java
@@ -1,12 +1,63 @@
+import java.util.Calendar; 
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 
-class Client{
+class ClientData{
 	private String password;
-	private Calendar RegisterDate;
+	private Calendar registerDate;
+
+	public ClientData(String password, Calendar registerDate){
+		this.password=password;
+		this.registerDate=new GregorianCalendar();
+	}
+
+	// devolve a password
+	public String getPassword(){
+		return this.password;
+	}
+
+	public Calendar getRegisterDate(){
+		return this.registerDate;   // this.registerDate.clone();
+  	}
+
 }
 
 class Server{
+	public static void main(String[] args){
 
+		HashMap<String, ClientData> clientsList = new HashMap<>();  // lista de clientes
+
+		// verificacao do numero de parametros de args
+		// É necessario a introduzao do numero da porta
+		if(args.length != 1){
+			System.out.println("Error! Necessário introduzir o numero da porta.");
+			System.exit(1);
+		}
+
+		try{
+			int port = Integer.parseInt(args[0]);  // string para int do num da porta
+		}
+		catch(NumberFormatException e){
+			System.out.println("Numero da porta tem de ser um numero inteiro");
+
+			//e.printStackTrace();
+		}
+
+		ServerSocket listener = new ServerSocket(port);  // socket, bind e listener
+
+
+		while(true){
+			Socket socket = listener.accept();
+		}
+
+	}
 
 
 }
